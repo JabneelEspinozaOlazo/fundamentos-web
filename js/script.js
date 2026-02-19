eade1853077be3b11a9b920e4051f5a1e8c71bf9
Jabneel Arely Espinoza Olazo <TU_CORREO_DE_GITHUB>
2026-02-19 01:04:03 -0400

    Estructura base del proyecto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Mostrar nombre en consola
+console.log("Jabneel Espinoza Olazo");
+
+// Capturar el botón
+const boton = document.querySelector("button");
+
+// Evento al hacer clic
+boton.addEventListener("click", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe
+    alert("¡Botón presionado!");
+});
